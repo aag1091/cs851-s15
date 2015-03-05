17046862cd97afa58c5f546ecd589481dd527648
Avinash Gosavi <aag1091@gmail.com>
2015-03-04 19:12:02 -0500

    added warc content

---
+++ assignment2/wget/getWARC.py
@@ -0,0 +1,21 @@
+import pymongo
+from pymongo import MongoClient
+import os
+
+client = MongoClient()
+db = client.url_of_set_http
+new_urls = open('new_urls.txt', 'w')
+urls = open('urls.txt', 'r')
+
+
+if __name__ == "__main__":
+  # tweets = db.tweet_data.find({"status_code": 200}).skip(0).limit(200)
+  # for tweet in tweets:
+  #   urls.write(str(tweet['url']) + '\n')
+  #	  os.system("wget '%s' --warc-file='%s'" % (tweet['url'], tweet['tweet_id']))
+
+  i = 0
+  for line in urls:
+  	i+=1
+  	name = "url-" + str(i)
+  	os.system("wget '%s' --warc-file='%s'" % (line, name))
